9f9290aec4aabb53e44f9d45c4afd11bb77337e6
Henry Bauer <github@henrybauer.com>
2016-09-16 13:37:17 +0300

    break into PartModule and Controller

--- GPOSpeedPump/GPOSpeedPump.cs
+++ GPOSpeedPump/GPOSpeedPump.cs
@@ -26,15 +26,11 @@ namespace GPOSpeedFuelPump
 {
 	public class GPOSpeedPump : PartModule
 	{
-		private readonly int _winId = new System.Random ().Next (0x00010000, 0x7fffffff);
-		private Rect _winPos = new Rect (Screen.width / 2, Screen.height / 2, 208, 16);
-		private bool _winShow;
-		private float _lastUpdate;
 		private const float Tolerance = (float)0.0001;
         
 		private Dictionary<string, int> _resourceFlags;
 
-		[KSPField (isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "Pump Level"), UI_FloatRange (minValue = 0f, maxValue = 16f, stepIncrement = 1f)]
+		[KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "Pump Level"), UI_FloatRange(minValue = 0f, maxValue = 16f, stepIncrement = 1f)]
 		public float _pumpLevel;
         
 		[KSPField (isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "Pump is "), UI_Toggle (disabledText = "Off", enabledText = "On")]
@@ -43,7 +39,7 @@ namespace GPOSpeedFuelPump
 		[KSPField (isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "Balance"), UI_Toggle (disabledText = "No", enabledText = "Yes")]
 		public bool _autoBalance;
 
-		private int GetResourceFlags (string resourceName, int mask)
+		public int GetResourceFlags (string resourceName, int mask)
 		{
 			try {
 			if (_resourceFlags == null)
@@ -68,7 +64,7 @@ namespace GPOSpeedFuelPump
 			}
 		}
 
-		private void SetResourceFlags (string resourceName, int value)
+		public void SetResourceFlags (string resourceName, int value)
 		{
 			if (_resourceFlags == null)
 				_resourceFlags = new Dictionary<string, int> ();
@@ -127,138 +123,13 @@ namespace GPOSpeedFuelPump
 			return true;
 		}
 
-		private void DrawConfigWindow (int id)
-		{
-			GUIStyle style = new GUIStyle (GUI.skin.button) { padding = new RectOffset (8, 8, 4, 4) };
-
-			GUILayout.BeginVertical ();
-			GUILayout.Label (part.partInfo.title);
-			foreach (PartResource pr in part.Resources) {
-				SetResourceFlags (pr.resourceName, GetResourceFlags (pr.resourceName, ~1) | (GUILayout.Toggle (GetResourceFlags (pr.resourceName, 1) == 1, "Pump " + pr.resourceName) ? 1 : 0));
-				SetResourceFlags (pr.resourceName, GetResourceFlags (pr.resourceName, ~2) | (GUILayout.Toggle (GetResourceFlags (pr.resourceName, 2) == 2, "Balance " + pr.resourceName) ? 2 : 0));
-			}
-			if (GUILayout.Button ("Close", style, GUILayout.ExpandWidth (true))) {
-				_winShow = false;
-			}
-			GUILayout.EndVertical ();
-		
-			GUI.DragWindow ();
-		}
-
-		internal static Rect clampToScreen (Rect rect)
-		{
-			rect.x = Mathf.Clamp (rect.x, 0, Screen.width - rect.width);
-			rect.y = Mathf.Clamp (rect.y, 0, Screen.height - rect.height);
-			return rect;
-		}
-
-		private void OnGUI ()
-		{
-			if (_winShow) {
-				GUI.skin = null;
-				_winPos = clampToScreen (GUILayout.Window (_winId, _winPos, DrawConfigWindow, "GPOSpeed Pump"));
-			}
-		}
-
 		[KSPEvent (guiActive = true, guiActiveEditor = true, guiName = "Pump Options")]
 		public void ConfigurePump ()
 		{
-			if (!_winShow) {
-				_winPos.xMin = Math.Min (Math.Max (0, vessel == null ? Screen.width - _winPos.width : Event.current.mousePosition.x + 180), Screen.width - _winPos.width);
-				_winPos.yMin = Math.Min (Math.Max (0, Event.current.mousePosition.y), Screen.height - _winPos.height);
-				_winPos.width = 208;
-				_winPos.height = 16;
-				_winShow = true;
-			} else {
-				_winShow = false;
-			}
+			GPOSpeedPumpController.Instance().ConfigurePump(part);
+
 		}
 
-		private void PumpOut (float secs)
-		{
-			foreach (PartResource pumpRes in part.Resources) {
-				if (pumpRes.flowState) { // don't operate if resource is locked
-					if (GetResourceFlags (pumpRes.resourceName, 1) == 1) {
-						foreach (Part shipPart in vessel.Parts) {
-							float shipPartLevel = 0f;
-							if (shipPart.Modules.Contains ("GPOSpeedPump")) {
-								var gpoSpeedPump = shipPart.Modules ["GPOSpeedPump"] as GPOSpeedPump;
-								if (gpoSpeedPump != null)
-									shipPartLevel = gpoSpeedPump._pumpLevel;
-							}
-							if (shipPartLevel < _pumpLevel) {
-								foreach (PartResource shipPartRes in shipPart.Resources) {
-									if (shipPartRes.resourceName == pumpRes.resourceName) {
-										if (shipPartRes.flowState) { // don't operate if resource is locked
-											double give = Math.Min (Math.Min (shipPartRes.maxAmount - shipPartRes.amount, pumpRes.amount), Math.Min (pumpRes.maxAmount, shipPartRes.maxAmount) / 10.0 * secs);
-											if (give > 0.0) { // Sanity check.  Apparently some other mods happily set amount or maxAmount to... interesting values...
-												pumpRes.amount -= give;
-												shipPartRes.amount += give;
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
 
-		private void Balance ()
-		{
-			foreach (PartResource pumpRes in part.Resources) {
-				if (pumpRes.flowState) { // don't operate if resource is locked
-					if (GetResourceFlags (pumpRes.resourceName, 2) == 2) {
-						double resAmt = 0f;
-						double resMax = 0f;
-						foreach (Part shipPart in vessel.Parts) {
-							if (shipPart.Modules.Contains ("GPOSpeedPump")
-							    && ((GPOSpeedPump)shipPart.Modules ["GPOSpeedPump"])._autoBalance
-							    && Math.Abs (((GPOSpeedPump)shipPart.Modules ["GPOSpeedPump"])._pumpLevel - _pumpLevel) < Tolerance) {
-								foreach (PartResource shipPartRes in shipPart.Resources) {
-									if (shipPartRes.resourceName == pumpRes.resourceName) {
-										if (shipPartRes.flowState) { // don't operate if resource is locked
-											resAmt += shipPartRes.amount;
-											resMax += shipPartRes.maxAmount;
-										}
-									}
-								}
-							}
-						}
-						foreach (Part shipPart in vessel.Parts) {
-							if (shipPart.Modules.Contains ("GPOSpeedPump")
-							    && ((GPOSpeedPump)shipPart.Modules ["GPOSpeedPump"])._autoBalance
-							    && Math.Abs (((GPOSpeedPump)shipPart.Modules ["GPOSpeedPump"])._pumpLevel - _pumpLevel) < Tolerance) {
-								foreach (PartResource shipPartRes in shipPart.Resources) {
-									if (shipPartRes.resourceName == pumpRes.resourceName) {
-										if (shipPartRes.flowState) { // don't operate if resource is locked
-											shipPartRes.amount = shipPartRes.maxAmount * resAmt / resMax;
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-
-		public override void OnUpdate ()
-		{
-			float now = Time.time;
-		
-			if (_autoPump && _pumpLevel > 0f)
-				PumpOut (now - _lastUpdate);
-		
-			if (_autoBalance)
-				Balance ();
-		
-			_lastUpdate = now;
-		
-			if (_winShow && !vessel.isActiveVessel) {
-				_winShow = false;
-			}
-		}
 	}
 }
---
+++ GPOSpeedPump/GPOSpeedPumpController.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using UnityEngine;
+
+namespace GPOSpeedFuelPump
+{
+
+	[KSPAddon(KSPAddon.Startup.FlightAndEditor, false)]
+	public class GPOSpeedPumpController : MonoBehaviour
+	{
+		private float _lastUpdate;
+		static private GPOSpeedPumpController instance;
+		private readonly int _winId = new System.Random().Next(0x00010000, 0x7fffffff);
+		private Rect _winPos = new Rect(Screen.width / 2, Screen.height / 2, 208, 16);
+		private bool _winShow;
+		private Part configPart = null;
+
+		public static GPOSpeedPumpController Instance()
+		{
+			if (instance == null)
+			{
+				GPOprint("making new instance");
+				instance = new GPOSpeedPumpController();
+			}
+			return instance;
+		}
+
+		static public void GPOprint(string tacos)
+		{
+			print("[GPOSpeedFuelPump]: " + tacos); // tacos are awesome
+		}
+
+		private GPOSpeedPumpController()
+		{
+			GPOprint("startup");
+			instance = this;
+		}
+
+		/*		private void PumpOut(Part part, float secs)
+				{
+					foreach (PartResource pumpRes in part.Resources)
+					{
+						if (pumpRes.flowState)
+						{ // don't operate if resource is locked
+							if (GetResourceFlags(pumpRes.resourceName, 1) == 1)
+							{
+								foreach (Part shipPart in vessel.Parts)
+								{
+									float shipPartLevel = 0f;
+									if (shipPart.Modules.Contains("GPOSpeedPump"))
+									{
+										var gpoSpeedPump = shipPart.Modules["GPOSpeedPump"] as GPOSpeedPump;
+										if (gpoSpeedPump != null)
+											shipPartLevel = gpoSpeedPump._pumpLevel;
+									}
+									if (shipPartLevel < _pumpLevel)
+									{
+										foreach (PartResource shipPartRes in shipPart.Resources)
+										{
+											if (shipPartRes.resourceName == pumpRes.resourceName)
+											{
+												if (shipPartRes.flowState)
+												{ // don't operate if resource is locked
+													double give = Math.Min(Math.Min(shipPartRes.maxAmount - shipPartRes.amount, pumpRes.amount), Math.Min(pumpRes.maxAmount, shipPartRes.maxAmount) / 10.0 * secs);
+													if (give > 0.0)
+													{ // Sanity check.  Apparently some other mods happily set amount or maxAmount to... interesting values...
+														pumpRes.amount -= give;
+														shipPartRes.amount += give;
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+
+				private void Balance(Part part)
+				{
+					foreach (PartResource pumpRes in part.Resources)
+					{
+						if (pumpRes.flowState)
+						{ // don't operate if resource is locked
+							if (GetResourceFlags(pumpRes.resourceName, 2) == 2)
+							{
+								double resAmt = 0f;
+								double resMax = 0f;
+								foreach (Part shipPart in vessel.Parts)
+								{
+									if (shipPart.Modules.Contains("GPOSpeedPump")
+										&& ((GPOSpeedPump)shipPart.Modules["GPOSpeedPump"])._autoBalance
+										&& Math.Abs(((GPOSpeedPump)shipPart.Modules["GPOSpeedPump"])._pumpLevel - _pumpLevel) < Tolerance)
+									{
+										foreach (PartResource shipPartRes in shipPart.Resources)
+										{
+											if (shipPartRes.resourceName == pumpRes.resourceName)
+											{
+												if (shipPartRes.flowState)
+												{ // don't operate if resource is locked
+													resAmt += shipPartRes.amount;
+													resMax += shipPartRes.maxAmount;
+												}
+											}
+										}
+									}
+								}
+								foreach (Part shipPart in vessel.Parts)
+								{
+									if (shipPart.Modules.Contains("GPOSpeedPump")
+										&& ((GPOSpeedPump)shipPart.Modules["GPOSpeedPump"])._autoBalance
+										&& Math.Abs(((GPOSpeedPump)shipPart.Modules["GPOSpeedPump"])._pumpLevel - _pumpLevel) < Tolerance)
+									{
+										foreach (PartResource shipPartRes in shipPart.Resources)
+										{
+											if (shipPartRes.resourceName == pumpRes.resourceName)
+											{
+												if (shipPartRes.flowState)
+												{ // don't operate if resource is locked
+													shipPartRes.amount = shipPartRes.maxAmount * resAmt / resMax;
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+		*/
+		public void ConfigurePump(Part p)
+		{
+			GPOprint("ConfigurePump()");
+			if ((!_winShow) || (p!=configPart))
+			{
+				_winPos.xMin = Math.Min(Math.Max(0, p.vessel == null ? Screen.width - _winPos.width : Event.current.mousePosition.x + 180), Screen.width - _winPos.width);
+				_winPos.yMin = Math.Min(Math.Max(0, Event.current.mousePosition.y), Screen.height - _winPos.height);
+				_winPos.width = 208;
+				_winPos.height = 16;
+				_winShow = true;
+				configPart = p;
+			}
+			else {
+				_winShow = false;
+				configPart = null;
+			}
+		}
+
+		internal static Rect clampToScreen(Rect rect)
+		{
+			rect.x = Mathf.Clamp(rect.x, 0, Screen.width - rect.width);
+			rect.y = Mathf.Clamp(rect.y, 0, Screen.height - rect.height);
+			return rect;
+		}
+
+		private void OnGUI()
+		{
+			if (_winShow)
+			{
+				GUI.skin = null;
+				_winPos = clampToScreen(GUILayout.Window(_winId, _winPos, DrawConfigWindow, "GPOSpeed Pump"));
+			}
+		}
+
+		public void FixedUpdate()
+		{
+			/*
+			if (_autoPump && _pumpLevel > 0f)
+				PumpOut(Time.time - _lastUpdate);
+
+			if (_autoBalance)
+				Balance();*/
+
+			_lastUpdate = Time.time;
+
+		}
+
+		public void OnAwake()
+		{
+			GameEvents.onVesselChange.Add(OnVesselSwitch);
+		}
+
+		public void OnVesselSwitch(Vessel v)
+		{
+			GPOprint("OnVesselSwitch()");
+			_winShow = false;
+		}
+
+		private void DrawConfigWindow(int id)
+		{
+			GUIStyle style = new GUIStyle(GUI.skin.button) { padding = new RectOffset(8, 8, 4, 4) };
+
+			GUILayout.BeginVertical();
+			GUILayout.Label(configPart.partInfo.title);
+			GPOSpeedPump pm = (GPOSpeedPump)configPart.Modules["GPOSpeedPump"];
+			foreach (PartResource pr in configPart.Resources)
+			{
+				pm.SetResourceFlags(pr.resourceName, pm.GetResourceFlags(pr.resourceName, ~1) | (GUILayout.Toggle(pm.GetResourceFlags(pr.resourceName, 1) == 1, "Pump " + pr.resourceName) ? 1 : 0));
+				pm.SetResourceFlags(pr.resourceName, pm.GetResourceFlags(pr.resourceName, ~2) | (GUILayout.Toggle(pm.GetResourceFlags(pr.resourceName, 2) == 2, "Balance " + pr.resourceName) ? 2 : 0));
+			}
+			if (GUILayout.Button("Close", style, GUILayout.ExpandWidth(true)))
+			{
+				_winShow = false;
+			}
+			GUILayout.EndVertical();
+
+			GUI.DragWindow();
+		}
+
+
+	}
+}
