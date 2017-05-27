8547b766159aa6d787c91882bc33aed46122c157
Henry Bauer <github@henrybauer.com>
2017-05-27 18:45:18 +0300

    bump to 1.8.8 for KSP 1.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.8.7")]
-[assembly: AssemblyFileVersion("1.8.7")]
+[assembly: AssemblyVersion("1.8.8")]
+[assembly: AssemblyFileVersion("1.8.8")]
