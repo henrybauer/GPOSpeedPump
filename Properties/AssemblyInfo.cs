1f3e57bd80bf6867d16c6bda0a0147f301e535be
Henry Bauer <github@henrybauer.com>
2018-03-31 17:31:24 +0300

    Bump version to 1.8.12

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.8.11")]
-[assembly: AssemblyFileVersion("1.8.11")]
+[assembly: AssemblyVersion("1.8.12")]
+[assembly: AssemblyFileVersion("1.8.12")]
