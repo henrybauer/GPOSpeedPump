bee3b7561e63b815323191c875884084915c730d
Henry Bauer <github@henrybauer.com>
2019-01-02 22:00:19 +0200

    * Recompiled for KSP 1.6

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.8.17")]
-[assembly: AssemblyFileVersion("1.8.17")]
+[assembly: AssemblyVersion("1.8.18")]
+[assembly: AssemblyFileVersion("1.8.18")]
